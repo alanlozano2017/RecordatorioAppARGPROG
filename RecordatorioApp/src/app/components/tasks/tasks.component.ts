bbf933e5b17cca492e588af2284c117de05066e2
Alan Lozano <alan.yo.99@gmail.com>
2022-05-11 20:44:07 -0300

    componente de lista de items

--- RecordatorioApp/src/app/components/tasks/tasks.component.ts
+++ RecordatorioApp/src/app/components/tasks/tasks.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { TASKS } from './../../mock-tasks';
 import { Task } from './../../Task';
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
